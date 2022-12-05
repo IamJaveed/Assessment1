078e432992a8becfd8cb208f672755293cb959e6
IamJaveed <119840463+IamJaveed@users.noreply.github.com>
2022-12-05 12:39:33 +0530

    Add files via upload

---
+++ doc0.java
@@ -0,0 +1,14 @@
+class doc0
+{
+	public static void main(String[]args)
+	
+	{
+		int value1=10;
+	    int value2=20;
+		
+		System.out.println(value1 == value2);
+		System.out.println(value1 != value2);
+		System.out.println(value1 != 20);
+	}
+}
+	
